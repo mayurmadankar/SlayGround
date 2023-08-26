b3ae60af4bfff66e22c5bd4e5f4486fa607f96f2
Mayur <madankarmayur5@gmail.com>
2023-08-26 09:46:28 +0530

    adding print name code day 14

---
+++ 14.Print_Name.java
@@ -0,0 +1,13 @@
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+    static void printName(String s){
+        // Write your code here.
+        int result = 0;
+        while(result<5){
+            System.out.println(""+s);
+            result++;
+        }
+    }
+}
